7e8c342734a9d143589f152ace1c56489d6d763c
MatthewJDavis <matthewdavis111@gmail.com>
2023-02-19 17:00:31 -0500

    add authn policy

---
+++ terraform/okta-authn-policy/main.tf
@@ -0,0 +1,36 @@
+# Only for OIE orgs.
+terraform {
+  required_providers {
+    okta = {
+      source = "okta/okta"
+      version = "~> 3.42"
+    }
+  }
+}
+
+# org_name, base_url, client_id and private_key are all set in environment variables.
+provider "okta" {
+  scopes = ["okta.policies.manage"]
+}
+
+
+resource "okta_app_signon_policy" "low_assurance" {
+  name        = "Low assurance Sign-On Policy"
+  description = "Authentication Policy for low assurance apps."
+}
+
+resource "okta_app_signon_policy_rule" "low_assurance" {
+  policy_id   = okta_app_signon_policy.low_assurance.id
+  name        = "Possession based phishing resistant."
+  factor_mode = "1FA"
+  constraints = [
+    jsonencode(
+    {
+      possession = {
+        deviceBound        = "REQUIRED"
+        phishingResistant = "REQUIRED"
+      }
+    }
+    )
+  ]
+}

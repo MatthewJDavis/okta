d0480dca491ab51e66b6ebc71e3804b59836e6a4
MatthewJDavis <matthewdavis111@gmail.com>
2023-02-19 15:49:00 -0500

    rename dir

---
+++ terraform/okta-applications/okta-oauth-application/README.md
@@ -0,0 +1,5 @@
+# Okta applications
+
+## OIDC Application
+
+OIDC created for a dotNet core application to authenticate with Okta.
---
+++ terraform/okta-applications/okta-oauth-application/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    okta = {
+      source = "okta/okta"
+      version = "~> 3.42"
+    }
+  }
+}
+
+# org_name, base_url, client_id and private_key are all set in environment variables.
+provider "okta" {
+  scopes = ["okta.apps.manage"]
+}
+
+resource "okta_app_oauth" "demo_app" {
+  label = "Sample DotNet OIDC"
+  type = "web"
+  consent_method = "REQUIRED"
+  login_uri = "https://localhost:5001/authorization-code/callback"
+  post_logout_redirect_uris = ["https://localhost:5001/signout-callback-oidc"]
+  redirect_uris = ["https://localhost:5001/signin-oidc"]
+  omit_secret = false
+  grant_types = ["authorization_code"]
+  response_types = ["code"]
+}
